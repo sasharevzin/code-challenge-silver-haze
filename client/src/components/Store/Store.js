5dfc8a1de7b233cc1383c70ea46e1712bfbbedb7
sasharevzin <sasha.nike@gmail.com>
2021-01-21 16:05:50 -0500

    Add store component

---
+++ client/src/components/Store/Store.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import "./Store.css"
+
+const Store = () => (
+  <div className="store">
+    <div className="container">
+      <div className="store--body">
+        <img className="store--image" src="https://placekitten.com/g/100/200" />
+        <div className="store--details">
+          <h2>Hank's Dank</h2>
+          <p>dad ad a a dad a</p>
+          <p>*****</p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Store
