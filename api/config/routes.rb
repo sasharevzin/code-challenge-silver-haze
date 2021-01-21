e744a443ac18ba706d1257e422c883d91075b430
sasharevzin <sasha.nike@gmail.com>
2021-01-21 14:54:13 -0500

    Define routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # NOTE: I can have it under `/api` namespace but I think it's fine
+  resources :users, only: :index do
+    resources :medical_recommendations, :state_ids
+  end
 end
