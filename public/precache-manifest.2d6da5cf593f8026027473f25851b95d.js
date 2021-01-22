1dd39338c4f00efc18f89cfeb3749500f1c5507d
sasharevzin <sasha.nike@gmail.com>
2021-01-21 23:12:54 -0500

    Make sure prod works properly - completed in 6 hours

---
+++ public/precache-manifest.2d6da5cf593f8026027473f25851b95d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "63bc196a8b7a01ccaa9c",
+    "url": "/static/js/main.63bc196a.chunk.js"
+  },
+  {
+    "revision": "03238436b83937b8706b",
+    "url": "/static/js/1.03238436.chunk.js"
+  },
+  {
+    "revision": "63bc196a8b7a01ccaa9c",
+    "url": "/static/css/main.0757a703.chunk.css"
+  },
+  {
+    "revision": "ede810961ecc0b46f501794884a68c6c",
+    "url": "/index.html"
+  }
+];
