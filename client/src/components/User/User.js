6a94d8c44eb6d6b8279234d871fa64f651c71835
sasharevzin <sasha.nike@gmail.com>
2021-01-21 21:57:25 -0500

    Add collapsing arrow

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -1,30 +1,48 @@
-import React from "react"
+import React, { useState } from "react"
 import MedicalRecommendation from "../MedicalRecommendation/MedicalRecommendation"
 import StateId from "../StateId/StateId"
 import Separator from "../Separator"
 
 import "./User.css"
 
-const User = ({ user }) => (
-  <div className="card p">
-    <div className="flex mb">
-      <img className="user--image" src="https://placekitten.com/g/50/50" />
-      <ul className="list stretch">
-        <li>
-          <h3>{user.name}</h3>
-        </li>
-        <li>
-          Email: {user.email}
-          <Separator />
-          Date of Birth: {user.date_of_birth}
-        </li>
-      </ul>
+const User = ({ user }) => {
+  const [open, setOpen] = useState(false)
+
+  const collapse = () => {
+    if (!open) return
+
+    return (
+      <div className="pt">
+        <MedicalRecommendation
+          medicalRecommendation={user.medical_recommendation}
+        />
+        <StateId stateId={user.state_id} />
+      </div>
+    )
+  }
+
+  return (
+    <div className="card p">
+      <div className="flex">
+        <img className="user--image" src="https://placekitten.com/g/50/50" />
+        <ul className="list stretch">
+          <li>
+            <h3>{user.name}</h3>
+          </li>
+          <li>
+            Email: {user.email}
+            <Separator />
+            Date of Birth: {user.date_of_birth}
+          </li>
+        </ul>
+        <span
+          className={open ? "arrow arrow__up" : "arrow arrow__down"}
+          onClick={() => setOpen(!open)}
+        />
+      </div>
+      {collapse()}
     </div>
-    <MedicalRecommendation
-      medicalRecommendation={user.medical_recommendation}
-    />
-    <StateId stateId={user.state_id} />
-  </div>
-)
+  )
+}
 
 export default User
